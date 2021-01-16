516b64aba1f69f557855aca7eea19e2c6bf40a4f
Christian Menzel <info@chrmenzel.de>
2021-01-16 22:39:11 +0100

    improved model with data augmentation

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from keras.layers import Convolution2D, MaxPooling2D
 lines = []
 
 # load driving_log.csv
-with open('./driving_data/data/driving_log.csv') as csvfile:
+with open('./driving_data/data/driving_log.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -16,9 +16,28 @@ with open('./driving_data/data/driving_log.csv') as csvfile:
 # remove header from lines
 lines = lines[1:]
 
-images = []
-measurements = []
+images, measurements = [], []
 
+correction = 0.2  # this is a parameter for the left and right images to tune the steering measurement
+
+print('start build measurements')
+for line in lines:
+    for i in range(3):
+        # Load images from center and left/right camera
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = './driving_data/data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+
+    # get steering measurements for this point in time
+    measurement = float(line[3])
+    measurements.append(measurements)
+    measurements.append(measurement + correction)
+    measurements.append(measurement - correction)
+
+print('measurements ready')
+    
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
@@ -30,10 +49,23 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-# Convert data to numpy arrays because that is the format Keras requires
-X_train = np.array(images)
-y_train = np.array(measurements)
+print('flip starts ...')
+# flip images horizontally to get more data for training
+augmented_images, augmented_measurements = [], []
 
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1)
+
+print('converting ... very critical process')
+# Convert data to numpy arrays because that is the format Keras requires
+X_train = np.array(augmented_images)
+print('X_train ready')
+y_train = np.array(augmented_measurements)
+print('y_train ready')
+print('converting ready')
 # build the model
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
