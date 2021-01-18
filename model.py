94da41a9c550cdaf4ee90e72a3a166b11e9873a0
Christian Menzel <info@chrmenzel.de>
2021-01-18 01:00:27 +0100

    model improved

--- model.py
+++ model.py
@@ -2,13 +2,15 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
 
+standard_path = '../../opt/carnd_p3/data/'
 lines = []
 
 # load driving_log.csv
-with open('./driving_data/data/driving_log.csv', 'r') as csvfile:
+with open(standard_path + 'driving_log.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -16,40 +18,47 @@ with open('./driving_data/data/driving_log.csv', 'r') as csvfile:
 # remove header from lines
 lines = lines[1:]
 
+print('There are ' + str(len(lines)) + ' lines in the csv file.')
 images, measurements = [], []
 
-correction = 0.2  # this is a parameter for the left and right images to tune the steering measurement
+correction = 0.3  # this is a parameter for the left and right images to tune the steering measurement
 
-print('start build measurements')
+# A safety check if pictures exist
+image = cv2.imread(standard_path + 'IMG/center_2016_12_01_13_30_48_287.jpg')
+if image is None:
+    print('image is null')
+    exit()
+
+print('start building measurements ...')
+
 for line in lines:
     for i in range(3):
         # Load images from center and left/right camera
         source_path = line[i]
         filename = source_path.split('/')[-1]
-        current_path = './driving_data/data/IMG/' + filename
+        current_path = standard_path + 'IMG/' + filename
         image = cv2.imread(current_path)
-        images.append(image)
+        if image is None:
+            print(current_path)
+            print("image not found from line " + str(i))
+            exit()
+        else:
+            images.append(image)
 
     # get steering measurements for this point in time
     measurement = float(line[3])
-    measurements.append(measurements)
-    measurements.append(measurement + correction)
-    measurements.append(measurement - correction)
-
-print('measurements ready')
-    
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = './driving_data/data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-
-    # get steering measurement for this point in time
-    measurement = float(line[3])
     measurements.append(measurement)
+    # correct only if measurement not too near at zero
+    if measurement >= -0.05 and measurement <= 0.05:
+        measurements.append(measurement)
+        measurements.append(measurement)
+    else:
+        measurements.append(measurement + correction)
+        measurements.append(measurement - correction)
 
-print('flip starts ...')
+print('measurements build')
+
+print('flip images starts ...')
 # flip images horizontally to get more data for training
 augmented_images, augmented_measurements = [], []
 
@@ -59,7 +68,8 @@ for image, measurement in zip(images, measurements):
     augmented_images.append(cv2.flip(image, 1))
     augmented_measurements.append(measurement * -1)
 
-print('converting ... very critical process')
+print('flip images finished')
+
 # Convert data to numpy arrays because that is the format Keras requires
 X_train = np.array(augmented_images)
 print('X_train ready')
@@ -68,30 +78,38 @@ print('y_train ready')
 print('converting ready')
 # build the model
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-
+model.add(Lambda(lambda x: (x / 127.5) - 1.0, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 """
-adding LeNet architecture: 2 convolutional layers with pooling layer
-and 3 fully connected layers
+adding NVIDIA architecture
 """
 
-# Convolutional layer 1 with pooling layer
-model.add(Convolution2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
-# Convolutional layer 2 with pooling layer
-model.add(Convolution2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
+# 5 Convolutional layers
+model.add(Convolution2D(24, (5, 5), subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(36, (5, 5), subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+#model.add(Dropout(.1))
+model.add(Convolution2D(48, (5, 5), subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(64, (3, 3), activation='relu'))
 
 model.add(Flatten())
 
-# 3 fully connected layers
-model.add(Dense(120))
-model.add(Dense(84))
+# 4 fully connected dense layers with dropout layers between
+model.add(Dense(100))
+model.add(Dropout(.1))
+model.add(Dense(50))
+model.add(Dropout(.1))
+model.add(Dense(10))
+model.add(Dropout(.1))
 model.add(Dense(1))
 
 # we use mean squared error (mse) because this is a regression network
 # and NOT a classification network
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
